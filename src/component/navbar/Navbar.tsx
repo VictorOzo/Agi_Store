1ab990f71a98d8901a62bde2238e69509d947a9c
Ozokwelu Victor <ozokweluvic@gmail.com>
2025-06-21 14:12:20 -0700

    authentication added

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -1,24 +1,48 @@
+"use client";
+import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
+import Form from "next/form";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import Agi_store from "@/assets/images/Agi_logo.png";
-import { Menu, User, Heart, ShoppingCart } from "lucide-react";
+import { Menu } from "lucide-react";
+import { PackageIcon, TrolleyIcon } from "@sanity/icons";
 
 export default function Navbar() {
+  const { user } = useUser();
   return (
     <header>
       <div className="bg-[#293544] text-gray-100 p-4 flex items-center justify-between px-[4vh]">
         <div className="text-2xl font-semibold">
-          <Image className="w-[70px] h-[70px]" src={Agi_store} alt="logo" />
+          <Link rel="stylesheet" href="/">
+            <Image className="w-[70px] h-[70px]" src={Agi_store} alt="logo" />
+          </Link>
         </div>
         <div className="hidden md:flex items-center gap-4">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="p-2 rounded-md text-white"
-          />
-          <User />
-          <Heart />
-          <ShoppingCart />
+          {/* Search area */}
+          <Form action="/search">
+            <input type="text" name="query" />
+          </Form>
+          {/* Cart Area */}
+          <Link href="/basket">
+            <TrolleyIcon className="w-7 h-7" />
+          </Link>
+          {/* Orders Area */}
+          <Link href="/orders">
+            <PackageIcon className="w-7 h-7" />
+          </Link>
+          {/* User Area */}
+          {user ? (
+            <div>
+              <UserButton />
+              <div>
+                <p>Welcome Back</p>
+                <p>{user.fullName}!</p>
+              </div>
+            </div>
+          ) : (
+            <SignInButton mode="modal" />
+          )}
         </div>
         <div className="md:hidden">
           <Menu />
