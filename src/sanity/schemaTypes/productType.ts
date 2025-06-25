78fb11c43658fbfd3b8e5dc30f6c068f62e61a24
Ozokwelu Victor <ozokweluvic@gmail.com>
2025-06-25 12:01:31 -0700

    product schema created

---
+++ src/sanity/schemaTypes/productType.ts
@@ -0,0 +1,76 @@
+import { TrolleyIcon } from "@sanity/icons";
+import { defineField, defineType } from "sanity";
+
+export const productType = defineType({
+  name: "productType",
+  title: "Product type",
+  type: "document",
+  icon: TrolleyIcon,
+  fields: [
+    defineField({
+      name: "name",
+      title: "Product Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "blockContent",
+      validation: (Rule) =>
+        Rule.max(200).warning("Description should be less than 200 characters"),
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: "price",
+      title: "Price",
+      type: "number",
+      validation: (Rule) =>
+        Rule.required().min(0).error("Price must be a positive number"),
+    }),
+    defineField({
+      name: "categorie",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: [{ type: "category" }] }],
+    }),
+    defineField({
+      name: "stock",
+      title: "Stock",
+      type: "number",
+      validation: (Rule) =>
+        Rule.required().min(0).error("Stock must be a non-negative number"),
+    }),
+  ],
+  preview: {
+    select: {
+      title: "name",
+      media: "image",
+      price: "price",
+    },
+    prepare(select) {
+      return {
+        title: select.title,
+        subtitle: `${select.price}`,
+        media: select.media,
+      };
+    },
+  },
+});
