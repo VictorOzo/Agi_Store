b8574374d652d9d464cdddb8d0a20c9f564f41c3
Ozokwelu Victor <ozokweluvic@gmail.com>
2025-06-30 14:19:11 -0700

    product schema and order schema created

---
+++ src/sanity/schemaTypes/salesType.ts
@@ -0,0 +1,65 @@
+import { TagIcon } from "@sanity/icons";
+import { defineField, defineType } from "sanity";
+
+export const salesType = defineType({
+  name: "sales",
+  title: "Sales",
+  type: "document",
+  icon: TagIcon,
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+    defineField({
+      name: "description",
+      title: "Sales Description",
+      type: "text",
+    }),
+    defineField({
+      name: "discountAmount",
+      type: "number",
+      title: "Discount Amount",
+      description: "Amount off in percentage or fixed value",
+    }),
+    defineField({
+      name: "couponCode",
+      title: "Coupon Code",
+      type: "string",
+    }),
+    defineField({
+      name: "ValidFrom",
+      title: "Valid From",
+      type: "datetime",
+    }),
+    defineField({
+      name: "validUntil",
+      title: "Valid Until",
+      type: "datetime",
+    }),
+    defineField({
+      name: "isActive",
+      title: "Is Active",
+      type: "boolean",
+      description: "Toggle to activate or deactivate the sale",
+      initialValue: true,
+    }),
+  ],
+  preview: {
+    select: {
+      title: "title",
+      discountAmount: "discountAmount",
+      couponCode: "couponCode",
+      isActive: "isActive",
+    },
+    prepare(selection) {
+      const { title, discountAmount, couponCode, isActive } = selection;
+      const status = isActive ? "Active" : "Inactive";
+      return {
+        title,
+        subtitle: `${discountAmount}% off - Code ${couponCode} ${status}`,
+      };
+    },
+  },
+});
